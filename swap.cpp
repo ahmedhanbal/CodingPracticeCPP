59b2607908c452dc386807af462caa7dd86c5537
ahmedhanbal <ahmedali.z24@outlook.com>
2024-08-03 10:43:20 +0500

    added swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    int num1, num2;
+    std::cout << "Enter two numbers to swap\n";
+    std::cout << "Num1: ";
+    std::cin >> num1;
+    std::cout << "Num2: ";
+    std::cin >> num2;
+
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num2 ^ num1;
+    std::cout << "Numbers swapped: " << num1 << ' ' << num2 << '\n';
+
+    return 0;
+}
